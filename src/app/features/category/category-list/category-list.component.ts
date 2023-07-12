4e8f5c95e67f3a9664a0226684e42d8c5e19cb92
satish saraskar <satishsaraskar@gmail.com>
2023-07-12 16:21:54 +0530

    using Async pipe to Subscribe to Observables

--- src/app/features/category/category-list/category-list.component.ts
+++ src/app/features/category/category-list/category-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../services/category.service';
 import { Category } from '../models/category.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-category-list',
@@ -9,18 +10,20 @@ import { Category } from '../models/category.model';
 })
 export class CategoryListComponent implements OnInit {
 
-  categories? : Category[];
-
+// categories? : Category[];
+//using async pipe
+categories$?:Observable<Category[]>;
 constructor( private categoryService:CategoryService){}
 
   ngOnInit(): void {
-  this.categoryService.getAllCategories()
-  .subscribe({
-    next:(response) =>{
-      console.log(response);
-      this.categories = response
-    }
-  })
+  this.categories$ = this.categoryService.getAllCategories()
+  // this.categoryService.getAllCategories()
+  // .subscribe({
+  //   next:(response) =>{
+  //     console.log(response);
+  //     this.categories = response
+  //   }
+  // })
   }
 
 }
